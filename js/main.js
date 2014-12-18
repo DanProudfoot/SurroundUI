b99ab48dce0d8403678f53d5fb09c465675b0da8
Dan Proudfoot <dan.proudfoot.iw@gmail.com>
2014-12-18 01:58:24 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+
+	var coverHolder = $('.cover_holder');
+	var coverColours = [1,2,3,4,5,6,7,8];
+
+	
+
+
+	// coverHolder.each( function(){
+	// 	coverHolder.addClass('grad_bg-' + coverColours[4]);
+	// 	console.log(Math.round(Math.random()*8));
+	// 
+
+});
