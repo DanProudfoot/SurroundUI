36193d8d03cde23b7b05d7cf5801ac900339ff91
Dan Proudfoot <dan.proudfoot.iw@gmail.com>
2014-12-19 18:47:41 +0000

    Lots of changes, more interactivity

--- js/main.js
+++ js/main.js
@@ -3,12 +3,34 @@ jQuery(document).ready(function($) {
 	var coverHolder = $('.cover_holder');
 	var coverColours = [1,2,3,4,5,6,7,8];
 
-	
+	function rand(){
+		return(
+			Math.floor(Math.random() * 8));
+	};
+
+	coverHolder.each(function(){
+		$(this).addClass('grad_bg-' + coverColours[rand()]);
+	});
 
 
-	// coverHolder.each( function(){
-	// 	coverHolder.addClass('grad_bg-' + coverColours[4]);
-	// 	console.log(Math.round(Math.random()*8));
-	// 
+	var sidebar = $('.sidebar');
+	var menuBtn = $('.menu_btn');
+	var mainView = $('.main_view');
+	var header = $('header');
+	var controlBar = $('.control_bar');
+
+
+	menuBtn.on('touchend click', function(e){
+		sidebar.toggleClass('open');
+		mainView.toggleClass('pad-left');
+		header.toggleClass('pad-left');
+		controlBar.toggleClass('pad-left');
+	});
+
+	var playlist = $('.playlist');
+
+	playlist.on('touchend click', function(e){
+		$(this).toggleClass('active');
+	});
 
 });
